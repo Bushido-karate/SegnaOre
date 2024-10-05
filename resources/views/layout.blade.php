fe84e325f7af3ac2995d23e1498a2a3aec8ae93e
FedericoRuocco <fede.ruocco@gmail.com>
2024-10-05 16:42:50 +0200

    auth

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>@yield('title', 'Blog')</title>
+	<title>@yield('title', 'Segna Ore')</title>
 	<!-- <link rel="stylesheet" href="{{url('css/main.css')}}"> -->
 	<link rel="stylesheet" href="{{asset('css/main.css')}}">
 </head>
@@ -12,6 +12,16 @@
 <body>
 	<ul class="nav">
 		<li><a class="{{request()->routeIs('home') ? 'active' : ''}}" href="{{route('home')}}">Home</a></li>
+
+		@auth
+		<li><a href="{{route('logout')}}">Logout</a></li>
+
+		<li class="username">
+			<p>Logged in as <b>{{Auth::user()->name}}</b></p>
+		</li>
+		@else
+		<li><a class="{{request()->routeIs('login') ? 'active' : ''}}" href="{{route('login')}}">Login</a></li>
+		@endauth
 	</ul>
 
 	@includeWhen($errors->any(), '_errors')
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'home'])->name('home');
+
+Route::match(['get', 'post'], '/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
